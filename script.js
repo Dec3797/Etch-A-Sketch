6a65b7192553daf1f5e8cbc69d164b93f41e623d
Dec <declan_meek@yahoo.com>
2023-06-20 18:16:48 +0100

    Add random colour generator function

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector('#container')
+const colourBackground = document.querySelectorAll('.grid')
 
 
 function createGrid(){
@@ -9,4 +10,16 @@ function createGrid(){
     }
 }
 
+function randomNumberGenerator(min, max){
+    return Math.floor(Math.random() * (max + 1))
+}
+
+function randomColourGenerator(min, max){
+    let red = randomNumberGenerator(0, 255);
+    let green = randomNumberGenerator (0, 255);
+    let blue = randomNumberGenerator (0, 255);
+    return [red, green, blue]
+}
+
 createGrid()
+
